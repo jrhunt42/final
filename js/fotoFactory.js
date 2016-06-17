abba00a66a99f3a1b4f5f48c87c820207a61cff7
jrhunt42 <jameshunt80303@gmail.com>
2016-06-17 07:53:16 +0000

    add some more static data for demo and fix carousel top fotos algorithm

--- js/fotoFactory.js
+++ js/fotoFactory.js
@@ -45,7 +45,7 @@ function fotoFactory() {
             while(this.fotos.length > 0) {
                   this.fotos.pop();
             }
-            //console.log("setCarouselFotos finished emptying carousel");
+            console.debug("setCarouselFotos finished emptying carousel");
             
             // iterate thru all users and each of their fotos,
             // keeping the three highest vote getters in the carousel fotos array
@@ -53,22 +53,31 @@ function fotoFactory() {
             
 
                   for(var foto in fotoGallery) {
-                        //console.log("setCarouselFotos checking next foto");
+                        //console.debug("setCarouselFotos checking next foto");
                         //always clear inCarousel flag until the end
                         fotoGallery[foto].inCarousel = false;
                         // until we have minimum number carousel fotos, just add each one
                         if(this.fotos.length < 3) {
-                              //console.log("setCarouselFotos inital push");
+                              //console.debug("setCarouselFotos inital push");
+                              console.debug("pushing:" + fotoGallery[foto].imgPath);
                               this.fotos.push(fotoGallery[foto]);
                               console.log(this.fotos);
                         } else {
                               //now we want to replace only if the current fotos voteCount
                               //is greater than one already in the carousel
+                              //we want to keep the carousel order by vote count so we always check/replace
+                              //lowest vote count foto
+                              // start with sorted carousel and reorder by vote count only if we change contents
+                              // rather than every time thru loop
+                              this.fotos.sort(fotoSortByVote);
                               for(var j=0; j< this.fotos.length; j++) {
                                     if(fotoGallery[foto].voteCount > this.fotos[j].voteCount) {
                                           //need to replace a foto
-                                          //console.log("replacing foto at index: " + j);
+                                          console.debug("replacing vote:::with" + this.fotos[j].voteCount +":::" +fotoGallery[foto].voteCount);
+                                          console.debug(this.fotos[j]);
+                                          console.debug(fotoGallery[foto]);
                                           this.fotos[j] = fotoGallery[foto];
+                                          this.fotos.sort(fotoSortByVote);
                                           break;
                                     }
                               }
@@ -90,43 +99,63 @@ function fotoFactory() {
             
       };
       
-      //sort carousel fotos by voteCount, highest votes first
-//      function fotoSortByVote (foto1, foto2) {
-//            if(foto1.voteCount < foto2.voteCount) {
-//                  return 1;
-//            }
-//            if(foto1.voteCount > foto2.voteCount) {
-//                  return -1;
-//            }
-//            // the vote counts are equal
-//            return 0;
-//      }
+      //sort carousel fotos by voteCount, lowest votes first
+      function fotoSortByVote (foto1, foto2) {
+            if(foto1.voteCount < foto2.voteCount) {
+                  return -1;
+            }
+            if(foto1.voteCount > foto2.voteCount) {
+                  return 1;
+            }
+            // the vote counts are equal
+            return 0;
+      }
       
       //create some static data
       // some foto objects
-      var johnFoto1 = new FotoObject("john", "https://cohort-work-jrhunt42.c9users.io/midterm/images/IMGP1249.JPG", "dummy caption",0,[],[],false, Date.now());
+      var johnFoto1 = new FotoObject("john", "https://cohort-work-jrhunt42.c9users.io/midterm/images/IMGP1249.JPG", "dummy caption",2,["craig","angie"],[],false, Date.now());
       var bikeFoto1 = new FotoObject("bikenut","https://cohort-work-jrhunt42.c9users.io/midterm/images/06-IMG_2100.JPG","dummy caption",0, [], [], false, Date.now());
       var bikeFoto2 = new FotoObject("bikenut","https://cohort-work-jrhunt42.c9users.io/midterm/images/IMGP0603.JPG","dummy caption",0,[], [], false, Date.now());
       var kelseyFoto1 = new FotoObject("kelsey","https://cohort-work-jrhunt42.c9users.io/midterm/images/IMGP0017.JPG", "dummy caption", 0, [],[],false,Date.now());
-      var kelseyFoto2 = new FotoObject("kelsey","https://cohort-work-jrhunt42.c9users.io/midterm/images/IMGP0473.JPG","dummy caption",0,[],[],false,Date.now());
+      var kelseyFoto2 = new FotoObject("kelsey","https://cohort-work-jrhunt42.c9users.io/midterm/images/IMGP0473.JPG","dummy caption",3,["craig","angie","john"],[],false,Date.now());
+      var mtndudeFoto1 = new FotoObject("mtndude","https://cohort-work-jrhunt42.c9users.io/midterm/images/DSC_0011.jpg","dummy caption",1,["craig"],[],false,Date.now());
+      var mtndudeFoto2 = new FotoObject("mtndude","https://cohort-work-jrhunt42.c9users.io/midterm/images/IMG_2573.jpg","dummy caption",0,[],[],false,Date.now());
+      var craigFoto1 = new FotoObject("craig","https://cohort-work-jrhunt42.c9users.io/midterm/images/IMGP1235.JPG","dummy caption",0,[],[],false,Date.now());
+      var craigFoto2 = new FotoObject("craig","https://cohort-work-jrhunt42.c9users.io/midterm/images/IMGP1211.JPG","dummy caption",0,[],[],false,Date.now());
+      var angieFoto1 = new FotoObject("angie","https://cohort-work-jrhunt42.c9users.io/midterm/images/IMGP0741.JPG","dummy caption",0,[],[],false,Date.now());
+      var angieFoto2 = new FotoObject("angie","https://cohort-work-jrhunt42.c9users.io/midterm/images/08-IMG_2063.JPG","dummy caption",1,["craig"],[],false,Date.now());
+      
+      
       // some users
       var john = new FotoUser("john", "pwjohn", "abc@gmail.com", "123-456-7890");
       var bikenut = new FotoUser("bikenut", "pwbikenut", "xyz@yahoo.com", "987-654-3210");
       var kelsey = new FotoUser("kelsey", "pwkelsey", "wtf@hotmail.com", "303-867-5309");
+      var mtndude = new FotoUser("mtndude", "pwmtndude", "stfu@aol.com", "666-666-6666");
+      var craig = new FotoUser("craig", "pwcraig", "rtfm@gmail.com", "234-567-8901");
+      var angie = new FotoUser("angie", "pwangie", "aaa@yahoo.com", "876-543-2109");
       
       //initial user list
       var fotoUsers = {
             "john":     john,
             "bikenut":  bikenut,
-            "kelsey":   kelsey
+            "kelsey":   kelsey,
+            "mtndude":  mtndude,
+            "craig":    craig,
+            "angie":    angie
       };
       
       var fotoGallery = [
             johnFoto1,
             bikeFoto1,
-            bikeFoto2,
             kelseyFoto1,
-            kelseyFoto2
+            bikeFoto2,
+            kelseyFoto2,
+            angieFoto1,
+            mtndudeFoto2,
+            craigFoto1,
+            angieFoto2,
+            craigFoto2,
+            mtndudeFoto1
             
       ];
             
